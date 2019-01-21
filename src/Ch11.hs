866ffe3c64cec3eab6fb42ee5ab2eaee7d7af767
Sérgio Souza Costa <prof.sergio.costa@gmail.com>
2019-01-21 14:41:59 -0300

    updating...

--- src/Ch11.hs
+++ src/Ch11.hs
@@ -23,5 +23,37 @@ instance Arbitrary Ternary where
 
 ----------------------------------------------------------------------
 
+-- Defined in ‘Test.QuickCheck.Arbitrary’ 
 --instance Arbitrary Char where
     --arbitrary = elements (['A'..'Z'] ++ ['a' .. 'z'] ++ " ~!@#$%^&*()")
+
+
+data Doc = Empty
+    | Char Char
+    | Text String
+    | Line
+    | Concat Doc Doc
+    | Union Doc Doc
+    deriving (Show,Eq)
+
+instance Arbitrary Doc where
+        arbitrary = do
+            n <- choose (1,6) :: Gen Int
+            case n of
+                 1 -> return Empty
+    
+                 2 -> do x <- arbitrary
+                         return (Char x)
+    
+                 3 -> do x <- arbitrary
+                         return (Text x)
+    
+                 4 -> return Line
+    
+                 5 -> do x <- arbitrary
+                         y <- arbitrary
+                         return (Concat x y)
+    
+                 6 -> do x <- arbitrary
+                         y <- arbitrary
+                         return (Union x y)
