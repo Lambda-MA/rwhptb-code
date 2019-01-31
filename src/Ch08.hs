7b8975b120830db2b80cc14a7d358d3ac425c7e9
Sérgio Souza Costa <prof.sergio.costa@gmail.com>
2019-01-31 16:40:17 -0300

    atualizando capitulo 8

--- src/Ch08.hs
+++ src/Ch08.hs
@@ -1,6 +1,7 @@
 module Ch08 where
 
 import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy.Char8 as L8
 
 hasElfMagic :: L.ByteString -> Bool
 hasElfMagic content = L.take 4 content == elfMagic
@@ -11,3 +12,29 @@ isElfFile path = do
   content <- L.readFile path
   return (hasElfMagic content)
   
+closing = readPrice . (!!4) . L8.split ','
+
+readPrice :: L8.ByteString -> Maybe Int
+readPrice str =
+    case L8.readInt str of
+      Nothing             -> Nothing
+      Just (dollars,rest) ->
+        case L8.readInt (L8.tail rest) of
+          Nothing           -> Nothing
+          Just (cents,more) ->
+            Just (dollars * 100 + cents)
+    
+highestClose :: L8.ByteString -> Maybe Int
+highestClose = maximum . (Nothing:) . map closing . L8.lines
+
+highestCloseFrom path = do
+    contents <- L8.readFile path
+    print (highestClose contents)
+
+
+f = do
+    s <- L8.readFile "prices.csv"
+    let s2 = L8.lines s
+    print $ closing $ head $tail s2
+
+f2 = L8.readFile "prices.csv" >>= (\s1 -> print $ L8.lines s1)
